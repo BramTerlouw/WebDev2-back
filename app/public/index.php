9c99e0a157a08a0685f81128d4967afdcab8955a
marbwoulret <bramterlouw12@gmail.com>
2022-03-06 17:05:17 +0100

    delete works

--- app/public/index.php
+++ app/public/index.php
@@ -1,7 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
-header("Access-Control-Allow-Methods: POST, PUT, GET, OPTIONS");
+header("Access-Control-Allow-Methods: POST, PUT, GET, DELETE, OPTIONS");
 
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
@@ -17,7 +17,7 @@ $router->get('/products', 'ProductController@getAll');
 $router->get('/products/(\d+)', 'ProductController@getOne');
 $router->post('/products', 'ProductController@create');
 $router->put('/products/(\d+)', 'ProductController@update');
-$router->delete('.products/(\d+)', 'ProductController@delete');
+$router->delete('/products/(\d+)', 'ProductController@delete');
 
 $router->get('/categories', 'CategoryController@getAll');
 $router->post('/stocks', 'StockController@create');
